78a1389b7f709a658b175f69889e86e3e93b76de
natewatkin21 <nate@assemble.tv>
2024-10-07 08:37:24 -0500

    Working version of app with Mac code signing and auto-updater

---
+++ scripts/notarize.js
@@ -0,0 +1,21 @@
+import { notarize } from '@electron/notarize';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+export default async function notarizing(context) {
+  const { electronPlatformName, appOutDir } = context;  
+  if (electronPlatformName !== 'darwin') {
+    return;
+  }
+
+  const appName = context.packager.appInfo.productFilename;
+
+  return await notarize({
+    tool: 'notarytool',
+    appPath: `${appOutDir}/${appName}.app`,
+    appleId: process.env.APPLE_ID,
+    appleIdPassword: process.env.APPLE_APP_SPECIFIC_PASSWORD,
+    teamId: process.env.APPLE_TEAM_ID
+  });
+}
